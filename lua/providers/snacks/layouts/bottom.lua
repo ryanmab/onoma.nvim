d3300757c20796405eef2e69339f223a645ad09b
Ryan Maber <ryan@ryanmaber.com>
2026-08-24 00:20:24 +0100

    Rework the `bottom` layout styles for Snacks picker

--- lua/providers/snacks/layouts/bottom.lua
+++ lua/providers/snacks/layouts/bottom.lua
@@ -6,23 +6,30 @@ function M.get_layout()
 		layout = {
 			reverse = true,
 			preview = 'main',
-			preset = 'bottom',
-		},
-		layouts = {
-			bottom = {
-				layout = {
-					box = 'vertical',
-					backdrop = false,
-					row = -1,
-					width = 0,
-					height = 0.25,
-					{ win = 'list', height = 1 },
-					{
-						box = 'horizontal',
-						{ win = 'list', border = 'none' },
-						{ win = 'preview', title = '{preview}' },
+			cycle = false,
+			layout = {
+				box = 'vertical',
+				backdrop = false,
+				row = -1,
+				width = 0,
+				height = 0.25,
+				{ win = 'preview', title = '{preview}' },
+				{
+					win = 'list',
+					border = { '', ' ', '', '', '', ' ', '', '' },
+					wo = {
+						-- Blend border into Picker background
+						winhighlight = 'FloatBorder:SnacksPicker',
 					},
-					{ win = 'input', height = 1, border = 'none' },
+				},
+				{ win = 'input', height = 1 },
+			},
+		},
+		win = {
+			preview = {
+				wo = {
+					-- Preserve the status line in the preview window
+					statusline = vim.o.statusline,
 				},
 			},
 		},
